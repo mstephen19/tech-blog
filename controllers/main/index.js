1ddeb9eb313f131ae7d87539dd16e687ed8b074c
Matt <matthiasvstephens@gmail.com>
2021-10-29 03:58:59 +0200

    Set up basic main.handlebars

--- controllers/main/index.js
+++ controllers/main/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 router.get('/'), async (req, res) => {
-  
+
 };
+
+// at the bottom create a wildcard that sends to a 404 if route doesn't exist
 
 module.exports = router;
