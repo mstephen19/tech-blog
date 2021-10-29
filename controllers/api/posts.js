af13e09e74cc945a73fc72a303bd10e06f41d196
Matt <matthiasvstephens@gmail.com>
2021-10-29 03:02:23 +0200

    Complete API routes

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -1,4 +1,60 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
-module.exports = route;
+router
+  .route('/')
+  .get(async (req, res) => {
+    const posts = await Post.findAll({
+      include: [{ model: User }, { model: Comment }],
+    });
+    res.status(200).json(posts);
+  })
+  .post(async (req, res) => {
+    const added = await Post.create({
+      user_id: req.body.user_id,
+      title: req.body.title,
+      description: req.body.description,
+    });
+    res.status(200).json(added);
+  });
+
+router
+  .route('/:id')
+  .get(async (req, res) => {
+    const post = await Post.findByPk(req.params.id, {
+      include: [{ model: User }, { model: Comment }],
+    });
+    res.status(200).json(post);
+  })
+  // Will probably never change user_id, but it is an option
+  .put(async (req, res) => {
+    const updated = await Post.update(
+      {
+        user_id: req.body.user_id,
+        title: req.body.title,
+        description: req.body.description,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(updated);
+  })
+  .delete(async (req, res) => {
+    const deleted = await Post.destroy({ where: { id: req.params.id } });
+    res.status(200).json(deleted);
+  });
+
+// Comment on a post
+router.post('/:id/comment', async (req, res) => {
+  const newComment = Comment.create({
+    comment: req.body.comment,
+    user_id: req.body.user_id,
+    post_id: req.params.id,
+  });
+  res.status(200).json(newComment);
+});
+
+module.exports = router;
--- controllers/api/users.js
+++ controllers/api/users.js
@@ -4,46 +4,87 @@ const { User, Post, Comment } = require('../../models');
 router
   .route('/')
   .get(async (req, res) => {
-    const users = await User.findAll({
-      include: [{ model: Post }, { model: Comment }],
-    });
-    res.status(200).json(users);
+    try {
+      const users = await User.findAll({
+        include: [{ model: Post }, { model: Comment }],
+      });
+
+      !users ? res.status(404).send(new Error('Oops!')) : null;
+
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   })
   .post(async (req, res) => {
-    const created = await User.create({
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password,
-    });
-    res.status(200).json(created);
+    try {
+      const created = await User.create({
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+      });
+
+      !created ? res.status(404).send(new Error('Oops!')) : null;
+
+      res.status(200).json(created);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   });
 
 router
   .route('/:id')
   .get(async (req, res) => {
-    const user = await User.findByPk(req.params.id, {
-      include: [{ model: Post }, { model: Comment }],
-    });
-    res.status(200).json(user);
+    try {
+      const user = await User.findByPk(req.params.id, {
+        include: [{ model: Post }, { model: Comment }],
+      });
+
+      !user ? res.status(404).send(new Error('Oops!')) : null;
+
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   })
   .put(async (req, res) => {
-    const updated = await User.update(
-      {
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password,
-      },
-      {
-        where: {
-          id: req.params.id,
+    try {
+      const updated = await User.update(
+        {
+          email: req.body.email,
+          username: req.body.username,
+          password: req.body.password,
         },
-      }
-    );
-    res.status(200).json(updated);
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      );
+
+      !updated ? res.status(404).send(new Error('Oops!')) : null;
+
+      res.status(200).json(updated);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   })
   .delete(async (req, res) => {
-    const deleted = await User.destroy({ where: { id: req.params.id } });
-    res.status(200).json(deleted);
+    try {
+      const deleted = await User.destroy({ where: { id: req.params.id } });
+      res.status(200).json(deleted);
+    } catch (err) {
+      res.status(400).json(err);
+    }
   });
+
+router.post('/:id/post', async (req, res) => {
+  const newPost = await Post.create({
+    title: req.body.title,
+    description: req.body.description,
+    user_id: req.params.id,
+  });
+  res.status(200).json(newPost);
+});
 
 module.exports = router;
