3f24abc7de58ef4a4def678a8adca3e10a239f89
Matt <matthiasvstephens@gmail.com>
2021-10-31 17:21:09 +0100

    Add delete post function

--- public/editPost.js
+++ public/editPost.js
@@ -1,10 +1,11 @@
+const postId = document.location.href[document.location.href.length - 1];
+
 const handleEditPost = async (e) => {
   e.preventDefault();
   const title = document.querySelector('#editPostForm input').value.trim();
   const description = document
     .querySelector('#editPostForm textarea')
     .value.trim();
-  const postId = document.location.href[document.location.href.length - 1];
 
   if (!title || !description || description.length < 20) {
     alert('Please fill out all the fields!');
@@ -21,6 +22,24 @@ const handleEditPost = async (e) => {
     ? document.location.replace('/dashboard')
     : alert('There was an error. Try again!');
 };
+
+const handleDeletePost = async () => {
+  const response = await fetch(`/api/posts/${postId}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    alert('Deleted!');
+    document.location.replace('/dashboard');
+  } else {
+    alert('There was an error deleting this post. Try again!');
+  }
+};
+
+document
+  .querySelector('#deleteBtn')
+  .addEventListener('click', handleDeletePost);
 
 document
   .querySelector('#editPostForm')
